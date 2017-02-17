35ea3ca84793e75f5a5616815433fc524c67f199
lfang40 <lfang40@gatech.edu>
2017-02-16 21:24:15 -0500

    Update index.php v0.02

--- index.php
+++ index.php
@@ -12,30 +12,49 @@
 </head>
 
 <body>
-
+	<!--Main Start-->
 	<div class="main_wrapper">
 	
+		<!--Head Start-->
 		<div class="head_wrapper">
-		asdb
-		</div>
 		
-		<div class="menu_wrapper">
-		menu
+			<img id="logo" src="images/logo.gif"> </img>
+			<img id="bogo" src="images/bogo.gif"></img>
+			
 		</div>
+		<!--Head End-->
+		
+		<!--Menu Start-->
+		<div class="menu_wrapper">
+			
+			<ul id="menu">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">All product</a></li>
+				<li><a href="#">My account</a></li>
+				<li><a href="#">Sign Up</a></li>
+				<li><a href="#">Shopping Cart</a></li>
+				<li><a href="#">Contact Us</a></li>
+			</ul>
+		
+		</div>
+		<!--Menu End-->
 	
+		<!--Content Start-->
 		<div class="content_wrapper">
 		
 			<div id="side"> side</div>
 			<div id="product"> product</div>
 		</div>
+		<!--Content End-->
 		
+		<!--Foot Start-->		
 		<div class="foot_wrapper">
 		foot
 		</div>
-	
+		<!--Foot End---->
 	
 	</div>
-
+	<!--Main End-->
 	
 	
 </body>
