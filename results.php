9b8745ee83d883b748212d986e37786e7a3e6927
lfang40 <lfang40@gatech.edu>
2017-02-21 08:56:58 -0500

    Update index.php, details,php, results.php,all_products.php V0.05

---
+++ results.php
@@ -0,0 +1,104 @@
+<!DOCTYPE>
+<?php 
+
+include("functions/functions.php");
+?>
+<html>
+
+<head>
+
+	<title>
+		Jupiter Bargin Online Shops
+	</title>
+	
+	<link rel="stylesheet" href="styles/style.css" media="all" />
+	
+</head>
+
+<body>
+	<!--Main Start-->
+	<div class="main_wrapper">
+	
+		<!--Head Start-->
+		<div class="head_wrapper">
+		
+			<a href="index.php"><img id="logo" src="images/logo.gif"> </img> </a>
+			<img id="bogo" src="images/bogo.gif"></img>
+			
+		</div>
+		<!--Head End-->
+		
+		<!--Menu Start-->
+		<div class="menu_wrapper">
+			
+			<ul id="menu">
+				<li><a href="index.php">Home</a></li>
+				<li><a href="all_products.php">All product</a></li>
+				<li><a href="customer/my_account.php">My account</a></li>
+				<li><a href="#">Sign Up</a></li>
+				<li><a href="cart.php">Shopping Cart</a></li>
+				<li><a href="#">Contact Us</a></li>
+			</ul>
+			
+			<div id="form">
+				<form method="get" action="results.php" enctype="multipart/form-data">
+					<input type="text" name="user_query" placeholder="Search a Product"/ > 
+					<input type="submit" name="search" value="Search" />
+				</form>
+			
+			</div>
+		
+		</div>
+		<!--Menu End-->
+	
+		<!--Content Start-->
+		<div class="content_wrapper">
+		
+			<div id="side"> 
+				
+				<div id="side_title">
+					Category
+				</div>
+				
+				<div id="category_title">
+				
+					<?php getCategory(); ?>
+					
+				</div>
+				
+				<div id="side_title">
+					Brand
+				</div>
+				
+				<div id="category_title">
+					
+					<?php getBrand(); ?>
+					
+				</div>
+			
+			</div>
+			
+			<div class="shopping_cart"> 
+			<h2>Shopping Cart</h2>
+			</div>
+			
+			<div class="product_wrapper"> 
+			<?php Search(); ?>
+		
+			</div>
+		</div>
+		<!--Content End-->
+		
+		<!--Foot Start-->		
+		<div class="foot_wrapper">
+			<h3 style="text-align:center; ">&copy; 2014 by www.OnlineTuting.com</h3>
+		</div>
+		<!--Foot End---->
+	
+	</div>
+	<!--Main End-->
+	
+	
+</body>
+
+</html>
